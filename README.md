17578638b72b019ed2cf60d923bdb09f3cb0133a
Dan O'Dea <danodea@gmail.com>
2022-03-12 21:51:53 -0500

    Initial commit
    contains a marketplace scraper and wantlist getter

---
+++ README.md
@@ -0,0 +1,6 @@
+## DISC-DEALS
+The Discogs wantlist UI sucks, so I am trying to make a better one. 
+
+Because I want to score sweet, sweet deals on emo vinyl.
+
+[Buy me something!](https://www.discogs.com/wantlist?user=danodea1)
---
+++ src/wantlist.js
@@ -0,0 +1,31 @@
+const Discogs = require('disconnect').Client;
+const fs = require('fs');
+const path = require('path');
+const debug = require('debug')('wantlist');
+
+const user = new Discogs().user();
+const masterIDs = new Set();
+const albums = new Set();
+
+const getFullWantlist = async function (page = 1) {
+    user.wantlist().getReleases('danodea1', {'page': page}, function(err, data){
+        processWantlistData(data.wants);
+        debug(data.pagination.page);
+
+        if (data.pagination.page < data.pagination.pages) {
+            getFullWantlist(data.pagination.page + 1)
+        } else {
+            fs.writeFileSync(path.join(__dirname, '/masterIDs.json'), JSON.stringify([...masterIDs]));
+            fs.writeFileSync(path.join(__dirname, '/albums.json'), JSON.stringify([...albums]));
+        }
+})};
+
+//expects the "wants" array from the api data
+const processWantlistData = function (wants) {
+    wants.forEach((want) => {
+        masterIDs.add(want.basic_information.master_id ? want.basic_information.master_id : want.basic_information.id);
+        albums.add(`${want.basic_information.artists[0].name} - ${want.basic_information.title.trim()}`);
+    })
+};
+
+getFullWantlist();
